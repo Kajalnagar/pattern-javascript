fc95c66d91d1b73968cd8b8b00e03c57af674c74
Kajalnagar <nagarkajal420@gmail.com>
2025-12-10 09:41:38 +0530

    Create pattern U.js

---
+++ pattern U.js
@@ -0,0 +1,20 @@
+let pattern = "";
+
+for (let i = 1; i <= 7; i++) {
+    for (let j = 1; j <= 7; j++) {
+
+        if (
+            j === 1 ||               // Left vertical line
+            j === 7 ||               // Right vertical line
+            i === 7 && j > 1 && j < 7 // Bottom horizontal line
+        ) {
+            pattern += "*";
+        } else {
+            pattern += " ";
+        }
+
+    }
+    pattern += "\n";
+}
+
+console.log(pattern);
