bec243102eb85b21ee1c22e28a25cd34501f288a
Kajalnagar <nagarkajal420@gmail.com>
2025-12-10 14:57:46 +0530

    Create triangle2.js

---
+++ triangle2.js
@@ -0,0 +1,17 @@
+let n = 5;
+
+for (let i = n; i >= 1; i--) {   
+    let row = "";
+
+    // spaces increase every row
+    for (let s = 1; s <= n - i; s++) {
+        row += " ";
+    }
+
+    // stars decrease every row
+    for (let j = 1; j <= i; j++) {
+        row += "*";
+    }
+
+    console.log(row);
+}
